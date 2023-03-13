7a60e4d6a276edbf2d2567bc616af18a6b82a41a
weng haishi <wenghaishi1@gmail.com>
2023-03-13 13:22:19 +0800

    bundle

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_12_070616) do
-  # These are extensions that must be enabled in order to support this database
+  # These are extensions that must be enabled in order to support  this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
